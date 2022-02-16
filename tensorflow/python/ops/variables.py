d12db4680010131eb38ef122620c922f500340d3
yuanman.ym <yuanman.ym@alibaba-inc.com>
2022-02-16 20:07:47 +0800

    [Embedding] Fix build break.

--- tensorflow/python/ops/variables.py
+++ tensorflow/python/ops/variables.py
@@ -33,7 +33,7 @@ from tensorflow.python.eager import context
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
-from tensorflow.lib.io import file_io
+from tensorflow.python.lib.io import file_io
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import gen_array_ops
